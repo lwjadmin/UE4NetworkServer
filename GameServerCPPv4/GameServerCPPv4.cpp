97013108617e587ad66f606a5d354b6d99ce0237
DESKTOP-0K0UJ4Q\ETVUSR <dndwpsla123@naver.com>
2022-12-28 14:27:34 +0900

    서버 제대로된 아이피로 수정!

--- GameServerCPPv4/GameServerCPPv4.cpp
+++ GameServerCPPv4/GameServerCPPv4.cpp
@@ -27,7 +27,7 @@
 WSAData                      NET_WSADATA = { 0, };
 SOCKET                       NET_SERVERSOCKET = NULL;
 SOCKADDR_IN                  NET_SERVERADDR = { 0, };
-const char*                  NET_SERVER_IPV4 = "127.0.0.1"; /*172.16.2.146*/
+const char*                  NET_SERVER_IPV4 = "172.16.2.146"; /*127.0.0.1*/
 const int                    NET_SERVER_PORT = 5001; /*5001*/
 const int                    NET_PACKET_SIZE = 512;
 std::map<SOCKET, ClientData> CLIENT_POOL;
@@ -36,8 +36,8 @@ CRITICAL_SECTION             CS_NETWORK_HANDLER;
 std::map<SOCKET, HANDLE>     THREAD_POOL;
 CRITICAL_SECTION             CS_THREAD_HANDLER;
 //--DBMS----------------------------------------------------------------------
-const std::string            DB_SERVERNAME = "tcp://127.0.0.1:3306"; /*172.16.2.146:3306*/
-const std::string            DB_USERNAME = "root"; /*AnimalGuysAdmin*/
+const std::string            DB_SERVERNAME = "tcp://172.16.2.146:3306"; /*172.16.2.146:3306*/
+const std::string            DB_USERNAME = "AnimalGuysAdmin"; /*AnimalGuysAdmin*/
 const std::string            DB_PASSWORD = "Passw0rd"; /*Passw0rd*/
 const std::string            DB_DBNAME = "ANIMALGUYS";
 sql::Driver*                 DB_DRIVER = nullptr;
